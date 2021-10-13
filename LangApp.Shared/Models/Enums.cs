51ae33c63993c62d4a4f6e1baa1b7dabd42e4f8a
Piotr Wyszyński <thevoux1@gmail.com>
2021-10-13 15:18:49 +0200

    feat(Shared): User, Enums, NotifyPropertyChanged

---
+++ LangApp.Shared/Models/Enums.cs
@@ -0,0 +1,11 @@
+﻿namespace LangApp.Shared.Models
+{
+    public class Enums
+    {
+        public enum UserRole
+        {
+            USER = 0,
+            ADMIN = 1
+        }
+    }
+}
---
+++ LangApp.Shared/Models/User.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using static LangApp.Shared.Models.Enums;
+
+namespace LangApp.Shared.Models
+{
+    public class User : NotifyPropertyChanged
+    {
+        private Guid _id;
+        public Guid Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _username;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private UserRole _userRole;
+        public UserRole UserRole
+        {
+            get
+            {
+                return _userRole;
+            }
+            set
+            {
+                _userRole = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
