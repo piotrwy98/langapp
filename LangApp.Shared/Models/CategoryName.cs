084521a2f39b4ff4002c8b98e52ed299d36d848f
Piotr Wyszyński <thevoux1@gmail.com>
2021-11-11 00:50:56 +0100

    feat(Shared): CategoryName added, FavouriteWord added

---
+++ LangApp.Shared/Models/CategoryName.cs
@@ -0,0 +1,10 @@
+﻿namespace LangApp.Shared.Models
+{
+    public class CategoryName
+    {
+        public uint Id { get; set; }
+        public Category Category { get; set; }
+        public Language Language { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ LangApp.Shared/Models/FavouriteWord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LangApp.Shared.Models
+{
+    public class FavouriteWord
+    {
+        public uint Id { get; set; }
+
+        public User User { get; set; }
+
+        public Word Word { get; set; }
+
+        public Language FirstLanguage { get; set; }
+
+        public Language SecondLanguage { get; set; }
+    }
+}
